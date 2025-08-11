14c923b674ad778e3f7b067ac566b41d17248d62
Amgenie <shailaghlois2009@gmail.com>
2025-08-11 22:11:47 +0800

    tweaked levels and added level transitions

--- levels/level_0.gd
+++ levels/level_0.gd
@@ -1,11 +1,16 @@
 extends Node
 
 @export var next_scene : String 
+@onready var animation_player = $AnimationPlayer
+@onready var level_complete = $"Level Complete"
+@onready var player = $Player
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Eventsbus.game_over.connect(_on_game_over)
 	Eventsbus.level_complete.connect(on_level_complete)
+	level_complete.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,5 +23,8 @@ func _on_game_over():
 
 func on_level_complete():
 	print("level finished!!!")
+	player.hide()
+	player.is_alive = false #just stops the player from moving
+	animation_player.play("transition")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file(next_scene)
-	
